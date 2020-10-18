fb9108931f1e51fe76322004cf4488fcb80524fd
Hideki Takada <hideki_tak@hotmail.com>
2020-10-19 08:18:35 +0900

    Import Test 2

--- SSZipArchive/ZipArchive.h
+++ SSZipArchive/ZipArchive.h
@@ -1,23 +1,23 @@
+////
+////  ZipArchive.h
+////  ZipArchive
+////
+////  Created by Serhii Mumriak on 12/1/15.
+////  Copyright © 2015 smumryak. All rights reserved.
+////
 //
-//  ZipArchive.h
-//  ZipArchive
+//#import <Foundation/Foundation.h>
 //
-//  Created by Serhii Mumriak on 12/1/15.
-//  Copyright © 2015 smumryak. All rights reserved.
+////! Project version number for ZipArchive.
+//FOUNDATION_EXPORT double ZipArchiveVersionNumber;
 //
-
-#import <Foundation/Foundation.h>
-
-//! Project version number for ZipArchive.
-FOUNDATION_EXPORT double ZipArchiveVersionNumber;
-
-//! Project version string for ZipArchive.
-FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/SSZipArchive.h>
-
-#if COCOAPODS
-#import <SSZipArchive/SSZipArchive.h>
-#else
-#import <SSZipArchive/SSZipArchive.h>
-#endif
+////! Project version string for ZipArchive.
+//FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/SSZipArchive.h>
+//
+//#if COCOAPODS
+//#import <SSZipArchive/SSZipArchive.h>
+//#else
+//#import <ZipArchive/SSZipArchive.h>
+//#endif
